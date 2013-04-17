777254023bf3f7a35319d5f6feb9fd68c08043e4
martinbang <elexer@gmail.com>
2013-04-17 11:36:21 +0200

    Map fungerer, kun satt opp zoom funksjoner

--- src/com/gpstracker/MainActivity.java
+++ src/com/gpstracker/MainActivity.java
@@ -1,15 +1,26 @@
 package com.gpstracker;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.gpstracker.map.TrackerMapActivity;
 
 public class MainActivity extends Activity {
-
+	
+	private Button btnMap;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //kun for testing
+        TestButtonForMap();
     }
 
 
@@ -20,4 +31,22 @@ public class MainActivity extends Activity {
         return true;
     }
     
-}
+    /**
+     * Kun For testing mot google map greia
+     */
+    public void TestButtonForMap(){
+    	
+    	btnMap = (Button) findViewById(R.id.buttonMap);
+    	btnMap.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(MainActivity.this,TrackerMapActivity.class);
+		        startActivity(i);
+			}
+		});
+    	
+    }//end method
+    
+}//end activity
--- src/com/gpstracker/map/TrackerMapActivity.java
+++ src/com/gpstracker/map/TrackerMapActivity.java
@@ -1,8 +1,41 @@
 package com.gpstracker.map;
 
+
+import android.os.Bundle;
+
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.gpstracker.R;
 
 public class TrackerMapActivity extends MapActivity {
+	
+	
+	private MapView mapView;
+	
+	private boolean setZoomeEnable = true;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.trackermapactivity_layout);
+		
+		mapView = (MapView) findViewById(R.id.map_view);
+		mapView.setBuiltInZoomControls(setZoomeEnable);
+		
+	}//end onCreate
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
